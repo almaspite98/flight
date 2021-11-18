8677164ba4c06af37cfd5482b37233112446b500
Török Attila <ifj.torok.attila@gmail.com>
2021-11-18 16:07:16 +0100

    Fixed "user not a managed type" error

--- src/main/java/org/openapitools/model/UserWithPreferences.java
+++ src/main/java/org/openapitools/model/UserWithPreferences.java
@@ -1,13 +1,20 @@
 package org.openapitools.model;
 
 import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
 
 @Data
 @Builder
 @With
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
+@Entity
+@Accessors(chain = true)
 public class UserWithPreferences {
+    @Id
     private String email;
     private String password;
     private String departure;
