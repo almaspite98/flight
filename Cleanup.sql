ab91a003a208b082d81759952e3e0f885aaa3acb
Török Attila <ifj.torok.attila@gmail.com>
2021-11-25 19:55:54 +0100

    Patched sql event

--- Cleanup.sql
+++ Cleanup.sql
@@ -1,15 +1,21 @@
+USE javabase;
 SET GLOBAL event_scheduler = "ON";
 DROP EVENT IF EXISTS javabase.CLEANUP;
-CREATE EVENT CLEANUP ON SCHEDULE EVERY 1 MINUTE
-STARTS CURRENT_TIMESTAMP ENDS CURRENT_TIMESTAMP + INTERVAL 10 YEAR ON COMPLETION PRESERVE ENABLE
-DO START TRANSACTION;
-    UPDATE flights AS F
+delimiter $$
+CREATE EVENT javabase.CLEANUP ON SCHEDULE EVERY 1 MINUTE
+STARTS CURRENT_TIMESTAMP ON COMPLETION PRESERVE ENABLE
+DO 
+begin
+START TRANSACTION;
+    UPDATE javabase.flights AS F
     SET number_of_seats = number_of_seats + (
-        SELECT COUNT(*) FROM reservations AS R
-        WHERE R.flight_id = F.flight_id AND ( R.status = 'FAILED' OR R.timestamp < NOW()-300 )
+        SELECT COUNT(*) FROM javabase.reservations AS R
+        WHERE R.flight_id = F.flight_id AND ( R.status = 'FAILED' OR R.timestamp < NOW()-60 )
     );
 
-    UPDATE reservations
+    UPDATE javabase.reservations
     SET status = 'CANCELLED'
-    WHERE status = 'FAILED' OR timestamp<NOW()-300;
+    WHERE status = 'FAILED' OR timestamp<NOW()-60;
 COMMIT;
+end $$
+delimiter ;
